9c55a81a734a06d5f48277ff036dd26400203153
= <=>
2020-05-31 11:55:52 +0200

    print, sort and reverse methods added

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::collections::LinkedList;
-use std::cmp;
+use std::{cmp, mem};
 use std::fmt::Debug;
-use std::ops::Deref;
 
 // Structures
 #[derive(Debug)]
@@ -19,7 +17,7 @@ pub struct Node<T> {
 // Iterator Structures
 pub struct IntoIter<T> (List<T>);
 
-pub struct Iter<'a,T> {
+pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
@@ -36,8 +34,8 @@ impl<T> List<T> {
         }
     }
 
-    pub fn push(&mut self, elem:T) {
-        self.head = Some(Box::new(Node{ elem: elem, next: self.head.take() }));
+    pub fn push(&mut self, elem: T) {
+        self.head = Some(Box::new(Node { elem: elem, next: self.head.take() }));
         self.size += 1;
     }
 
@@ -47,7 +45,7 @@ impl<T> List<T> {
             Some(node) => {
                 self.head = node.next;
                 Some(node.elem)
-            },
+            }
             None => None,
         }
         // self.head.take().map(|node|{
@@ -64,14 +62,14 @@ impl<T> List<T> {
         match self.head.as_ref() {
             Some(node) => {
                 Some(&node.elem)
-            },
+            }
             None => None
         }
     }
 
     // Iterator Methods
     pub fn into_iter(self) -> IntoIter<T> {
-        IntoIter (self)
+        IntoIter(self)
     }
 
     pub fn iter(&self) -> Iter<T> {
@@ -93,6 +91,10 @@ trait Iterator {
     fn next(&mut self) -> Option<Self::Item>;
 }
 
+pub trait Drop{
+    fn drop(&mut self);
+}
+
 // Traits Implementation
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
@@ -107,7 +109,7 @@ impl<'a, T> Iterator for Iter<'a, T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next.map(|node| {
-            self.next = node.next.as_ref().map(|next_node| { &** next_node });
+            self.next = node.next.as_ref().map(|next_node| { &**next_node });
             &node.elem
         })
     }
@@ -118,12 +120,20 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next.take().map(|node| {
-            self.next = node.next.as_mut().map(|next_node| { &mut ** next_node });
+            self.next = node.next.as_mut().map(|next_node| { &mut **next_node });
             &mut node.elem
         })
     }
 }
 
+impl <T> Drop for List<T>{
+    fn drop(&mut self){
+        let mut current = self.head.take();
+        while  let Some(mut boxed_node) = current {
+            current = boxed_node.next.take();
+        }
+    }
+}
 // Doubly Linked List
 #[derive(Debug)]
 pub struct DoublyLinkedList<T> {
@@ -133,7 +143,6 @@ pub struct DoublyLinkedList<T> {
 }
 
 impl<T> DoublyLinkedList<T> {
-
     pub fn new() -> Self {
         let mut left_list = List::new();
         let mut right_list = List::new();
@@ -144,15 +153,21 @@ impl<T> DoublyLinkedList<T> {
         }
     }
 
+    fn check_size(&mut self,index:i32){
+        if(self.size()==0 || self.size()<=index){
+            panic!("Index Out of Bounds!!!");
+        }
+    }
+
     pub fn next(&mut self) {
         if (self.right.get_size() != 0) {
-            self.right.pop().map(|node| { self.left.push(node); } );
+            self.right.pop().map(|node| { self.left.push(node); });
         }
     }
 
     pub fn previous(&mut self) {
         if (self.left.get_size() != 0) {
-            self.left.pop().map( |node| { self.right.push(node); } );
+            self.left.pop().map(|node| { self.right.push(node); });
         }
     }
 
@@ -175,6 +190,7 @@ impl<T> DoublyLinkedList<T> {
     }
 
     pub fn push(&mut self, elem: T, index: i32) {
+        self.check_size(index);
         self.shift(index);
         self.left.push(elem);
         self.size += 1;
@@ -187,6 +203,7 @@ impl<T> DoublyLinkedList<T> {
     }
 
     pub fn pop(&mut self, index: i32) {
+        self.check_size(index);
         self.shift(index);
         self.left.pop();
         self.size = cmp::max(0, self.size - 1);
@@ -200,9 +217,14 @@ impl<T> DoublyLinkedList<T> {
         self.left.get_top()
     }
 
-    pub fn get(&mut self, index: i32) -> Option<&T> {
+    pub fn get_current_position(&mut self) -> i32 {
+        self.left.get_size()
+    }
+
+    pub fn get(&mut self, index: i32) -> &T {
+        self.check_size(index);
         self.shift(index);
-        self.left.get_top()
+        self.left.get_top().unwrap()
     }
 
     pub fn edit_current(&mut self, value: T) {
@@ -211,66 +233,177 @@ impl<T> DoublyLinkedList<T> {
     }
 
     pub fn edit(&mut self, index: i32, value: T) {
+        self.check_size(index);
         self.shift(index);
         self.left.pop();
         self.left.push(value);
     }
 
+    pub fn size(&mut self) -> i32 {
+        *self.get_size().unwrap()
+    }
+
+    pub fn sort (&mut self)
+        where T: std::cmp::PartialEq + std::cmp::PartialOrd + std::fmt::Debug
+    {
+        self.shift(0);
+        let mut exp = 2;
+        let mut buffer:List<T> = List::new();
+        let size = self.size();
+        while ((exp>>1)  < size ){
+            let init = (exp>>1) - 1;
+            self.shift(init);
+            for idx in init..size {
+                self.shift(idx);
+                if( (idx - init) % exp == 0){
+                    let mut l = self.left.pop();
+                    let mut r = self.right.pop();
+                    let mut q = (exp>>1);
+                    while (l>=r  && q>0){
+                        match l.take(){
+                            Some(value) => {
+                                buffer.push(value);
+                            },
+                            None => {
+                                break;
+                            }
+                        }
+                        l = self.left.pop();
+                        q-=1;
+                    }
+                    if(l!=None){
+                        l.take().map(|value|{self.left.push(value);});
+                    }
+                    let mut b = buffer.pop();
+                    q = (exp>>1);
+                    while(b!=None){
+                        if(q>0 && r!=None && r<b){
+                            match r.take(){
+                                Some(value) => {
+                                    self.left.push(value);
+                                },
+                                None => {
+                                    break;
+                                }
+                            }
+                            r=self.right.pop();
+                            q-=1;
+                        }
+                        else{
+                            match b.take(){
+                                Some(value) => {
+                                    self.left.push(value);
+                                },
+                                None => {
+                                    break;
+                                }
+                            }
+                            b = buffer.pop();
+                        }
+                    }
+                    if(r!=None){
+                        r.take().map(|value|{self.right.push(value);});
+                    }
+                }
+
+            }
+            // self.print_all();
+            exp = exp<<1;
+        }
+    }
+    pub fn reverse(&mut self){
+        mem::swap(&mut self.left,&mut self.right);
+    }
+    pub fn print_all(&mut self)
+        where T: std::fmt::Debug
+    {
+        let sz = self.size();
+        for x in 0..sz {
+            print!("{:?} ", self.get(x));
+        }
+        println!();
+    }
+
 }
 
 pub fn print_dll(dl: &mut DoublyLinkedList<i32>) {
-    let sz = *dl.get_size().unwrap();
+    let sz = dl.size();
     for x in 0..sz {
-        print!("{} ", dl.get(x).unwrap());
+        print!("{:?} ", dl.get(x));
     }
 }
 
+#[derive(Debug,PartialEq, PartialOrd)]
+pub struct Person {
+    name: String,
+    age: u8,
+    height : u8
+}
 fn main() {
-    // let mut list:List<i32> = List::new();
-    // list.push(1);
-    // list.push(2);
-    // list.push(3);
-    //
-    // let mut it = list.iter_mut();
-    //
-    // let mut elem = it.next();
-    // elem.take().map(|node| {
-    //     *node += 2;
-    //     elem = Some(node);
-    // });
-    // it = list.iter_mut();
-    // elem = it.next();
-    // // print!("{} ", elem.unwrap());
-    // while (elem != None) {
-    //     elem.map(|value| {
-    //         println!("{}", value);
-    //     });
-    //     elem = it.next();
-    // }
 
     let mut dl: DoublyLinkedList<i32> = DoublyLinkedList::new();
-    dl.push_back(1);
-    dl.push_back(2);
-    dl.push_back(3);
-    dl.push_back(4);
     dl.push_back(5);
+    dl.push_back(3);
     dl.push_back(6);
+    dl.push_back(7);
+    dl.push_back(4);
+    dl.push_back(2);
+    dl.push_back(1);
+    dl.push_back(1123);
+    dl.push_back(231);
+    dl.push_back(1234);
+    dl.push_back(441);
+    dl.push_back(651);
+    dl.push_back(731);
+    dl.push_back(14);
+    dl.push_back(51);
+    dl.push_back(771);
+    dl.push_back(8881);
+    dl.push_back(31);
+    dl.print_all();
+    dl.reverse();
+    dl.print_all();
+    dl.sort();
+    dl.reverse();
+    dl.sort();
+    dl.print_all();
 
-    dl.previous();
-    dl.previous();
-    dl.previous();
-    dl.next();
-    dl.edit(1, 22);
-    print_dll(&mut dl);
+    // let person1 = Person{
+    //     name:String::from("John"),
+    //     age:25,
+    //     height:2
+    // };
+    // let person2 = Person{
+    //     name:String::from("John"),
+    //     age:25,
+    //     height:2
+    // };
 
-
-
-    // match dl.get_size() {
-    //     Some()
-    // }
-
-
-
+    // let mut dl: DoublyLinkedList<Person> = DoublyLinkedList::new();
+    // dl.push_back(Person {
+    //     name: "nick",
+    //     age: 15,
+    // });
+    // dl.push_back(Person {
+    //     name: "John",
+    //     age: 22,
+    // });
+    // dl.push_back(Person {
+    //     name: "Pablo",
+    //     age: 33,
+    // });
+    // dl.push_back(Person {
+    //     name: "Stefan",
+    //     age: 14,
+    // });
+    // dl.push_back(Person {
+    //     name: "Jose",
+    //     age: 88,
+    // });
+    // dl.push_back(Person {
+    //     name: "Jake",
+    //     age: 44,
+    // });
 }
 
 
